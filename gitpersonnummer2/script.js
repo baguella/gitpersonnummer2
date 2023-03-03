732fb0e2930a2bb02153c353317da2cfabac9338
Axel Lindström <axli0926@storsjogymnasiet.se>
2023-03-03 10:35:44 +0100

    klar

---
+++ gitpersonnummer2/script.js
@@ -0,0 +1,10 @@
+
+const psvar = dicyment.getElementById("svaret")
+
+
+function getPersonNr() {
+    const slumptal = Math.random() * 2000;
+
+    let responsearray = response.results;
+    console.log(responsearray[0])
+}
